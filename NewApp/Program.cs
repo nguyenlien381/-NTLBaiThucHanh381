8729cfbd704a25c40c46536e35da4cee3bbbeb70
nguyenlien381 <97586322+nguyenlien381@users.noreply.github.com>
2023-07-11 09:06:37 +0700

    Add files via upload

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO.Pipes;
+using NewApp.Models;
+public class Sinhvien
+{
+    public static void Main(string[] args)
+    {
+        Sinhvien ps = new Sinhvien();
+        string Ht = "Nguyen Thi Lien";
+        int Sdt = 093625345;
+        int Tuoi = 21;
+        System.Console.WriteLine("{0} - {1} -{2}",Ht,Sdt,Tuoi);
+    }
+    
+}
