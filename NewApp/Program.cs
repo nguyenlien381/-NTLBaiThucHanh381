d5b86e31d6f7495295ce93342e39114c1a0edd98
nguyenlien381 <97586322+nguyenlien381@users.noreply.github.com>
2023-07-11 20:58:40 +0700

    Add files via upload

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,19 +1,11 @@
-﻿public class Giaiphuongtrinh
+﻿
+using NewApp.Models;
+public class Program
 {
-     public static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Giaiphuongtrinh ps = new Giaiphuongtrinh();
-            int a = 10; 
-            int b = 5;
-            int c = 8;
-            double x;
-            float delta;
-            float x1;
-            float x2;
-            if(a != 0)
-            {
-                x = (double) - b / a;
-                 System.Console.WriteLine("Phuong trinh co nghiem x = {0}", Math.Round(x,2));
-            }
+        Student std = new Student();
+        std.EnterData();
+        std.Display();
     }
 }
